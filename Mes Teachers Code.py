f97dbb56e16dd034803616a67d60af15bd6c342f
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-11-04 18:07:58 +0530

    first commit

---
+++ Mes Teachers Code.py
@@ -0,0 +1,101 @@
+import pyperclip
+
+x = input('Enter the Initial of the Teacher:  ')
+
+# def coreFunction(letter,ID):    //Gonna be the driving function 
+#    if(x.upper() == ID)
+#     return True
+#    else 
+#      return False
+   
+
+# coreFunction("SV",324243234234) 
+
+
+# Gonna be replaced with Switch case(Dictionaries in Python)
+if x.upper() == 'SV':
+    print("ID: 7931380267")
+    pyperclip.copy("7931380267")
+
+elif  x.upper() == 'JP' :
+    print("ID: 7644991204")
+    pyperclip.copy("7644991204")
+
+elif  x.upper() == 'STP':
+    print("9257726878")
+    pyperclip.copy(" 9257726878")
+
+elif  x.upper() == 'SNK':
+    print("ID: 4302909682")
+    pyperclip.copy(" 4302909682")
+
+elif  x.upper() == 'AH':
+    print("ID: 4691762381")
+    pyperclip.copy(" 4691762381")
+
+elif  x.upper() == 'DCK':
+    print("ID: 5239834372")
+    pyperclip.copy("5239834372")
+
+elif  x.upper() == 'BS':
+    print("ID:71616553872")
+    pyperclip.copy("71616553872")
+
+elif  x.upper() == 'VV':
+    print("ID:7415289636")
+    pyperclip.copy("7415289636")
+
+elif  x.upper() == 'SKG':
+    print("ID: 9280360715")
+    pyperclip.copy(" 9280360715")
+
+elif  x.upper() == 'PB':
+    print("ID: 8938976636")
+    pyperclip.copy("  8938976636")
+
+elif  x.upper() == 'SP':
+    print("ID:4699455972")
+    pyperclip.copy("4699455972")
+    
+elif  x.upper() == 'TP':
+    print("ID:8469906757")
+    pyperclip.copy("8469906757")
+     
+elif  x.upper() == 'SS':
+    print("ID:9875924847")
+    pyperclip.copy("9875924847")
+
+elif  x.upper() == 'KNC':
+    print("5349366922")
+    pyperclip.copy("5349366922")
+
+elif  x.upper() == ' UD':
+    print("ID: 896628553")
+    pyperclip.copy(" 8966028553")
+
+elif  x.upper() == ' VRL':
+    print("ID:6644191114")
+    pyperclip.copy("6644191114")
+
+elif  x.upper() == 'SH':
+    print( "ID:8234694854 ")   
+    pyperclip.copy("8234694854")
+
+elif  x.upper() == ' VR':
+    print( "ID: 8376828015")
+    pyperclip.copy("8376828015")
+
+elif  x.upper() == ' GS':
+    print( "ID: 6507378241")
+    pyperclip.copy("6507378241")
+
+elif  x.upper() == 'KGR':
+    print( "ID: 9100203254")
+    pyperclip.copy("9100203254")
+
+elif  x.upper() == ' RS':
+    print( "ID: 76154545722")
+    pyperclip.copy("76154545722")
+
+else:
+    print("Sorry Unknown Initial")
