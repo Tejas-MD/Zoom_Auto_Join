fe6ad1650817068995749ebf911d006dac73cd91
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-11-04 19:01:36 +0530

    First but Best Commit, 1. Clean Code 2. Refactored into diff files 3. Print Statements 4. if else replaced with dictionaries 5. Functions added so that adding features is easy

---
+++ ExtrasNotCode/TODOS.md
@@ -0,0 +1,9 @@
+#TODOS: 
+
+#Clean Code so changes are easy to make
+
+#Auto Open Zoom after copying to clipboard 
+
+#Wait for user clicking + and turn off video and Paste ID 
+
+ 
---
+++ meetCodeGiverF.py
@@ -0,0 +1,34 @@
+import pyperclip 
+
+def meetCodeGiver(argument):    #PROCESSING
+    switcher = { 
+        "SV": 7931380267, 
+        "JP": 7644991204, 
+        "STP": 9257726878, 
+        "SNK": 4302909682, 
+        "AH":  4691762381, 
+        "DCK": 5239834372, 
+        "BS": 71616553872, 
+        "VV": 7415289636, 
+        "SKG": 9280360715, 
+        "PB": 8938976636, 
+        "SP": 4699455972,   
+        "TP": 8469906757, 
+        "SS": 9875924847, 
+        "KNC": 5349366922,
+        "UD": 8966028553, 
+        "VRL": 6644191114, 
+        "SH": 8234694854, 
+        "VR": 8376828015,  
+        "GS": 6507378241,
+        "KGR": 9100203254, 
+        "RS": 76154545722, 
+       } #Table Complete 
+  
+    # get() method of dictionary data type returns  
+    # value of passed argument if it is present  
+    # in dictionary otherwise second argument will 
+    # be assigned as default value of passed argument 
+    return switcher.get(argument, "Didn't find a match. Check the Inital Again Maybe?") 
+
+
