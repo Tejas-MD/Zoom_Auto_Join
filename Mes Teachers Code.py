ea19668567c7b4de7e016b11c61815d012e7e030
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-11-05 13:14:57 +0530

    Doesn't close until I close now. Looped.

--- Mes Teachers Code.py
+++ Mes Teachers Code.py
@@ -2,17 +2,17 @@ from meetCodeGiverF import meetCodeGiver
 import pyperclip 
 import subprocess
 
-
-x = input('Enter the Initial of the Teacher:  ')  #INPUT
-x= x.upper()
-
-
-print("Finding the Meeting Code, Just a Second...") #PROCESSING
-outputCode = meetCodeGiver(x)
+while(True):
+    x = input('Enter the Initial of the Teacher:  ')  #INPUT
+    x= x.upper()
 
 
-print (meetCodeGiver(x))    #OUTPUT 
-pyperclip.copy(outputCode)
-print("Code Copied to Clipboard, Opening Zoom, you're welcome :-)")
-subprocess.Popen("C:\\Users\\home\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
-#All the TODOS, any additional features, will have to be coded here. 
+    print("Finding the Meeting Code, Just a Second...") #PROCESSING
+    outputCode = meetCodeGiver(x)
+
+
+    print (meetCodeGiver(x))    #OUTPUT 
+    pyperclip.copy(outputCode)
+    print("Code Copied to Clipboard, Opening Zoom, you're welcome :-)")
+    subprocess.Popen("C:\\Users\\home\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+    #All the TODOS, any additional features, will have to be coded here. 
