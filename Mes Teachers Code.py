98bbd03e016f77444cc7379d29f56392594f5bd5
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-11-06 12:17:15 +0530

    Added Rejoin Functionality. Working!!

--- Mes Teachers Code.py
+++ Mes Teachers Code.py
@@ -8,19 +8,36 @@ while(True):
     x = input('Enter the Initial of the Teacher:  ')  #INPUT
     x= x.upper()
 
-
     print("Finding the Meeting Code, Just a Second...") #PROCESSING
     outputCode = meetCodeGiver(x)
-
-
+   
     print (meetCodeGiver(x))    #OUTPUT 
     pyperclip.copy(outputCode)
-    
     print("Code Copied to Clipboard, Opening Zoom, you're welcome :-)")
     subprocess.Popen("C:\\Users\\home\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
     time.sleep(7)
     keyboard.write(str(outputCode))  #Keyboard can only iterate strings!
     print("Pasting the Code")
     time.sleep(7)
+    print('Here goes the password:')
+    time.sleep(3)
     keyboard.write("mesmpl")
+
+    c = input('Wanna Rejoin? y/n')
+    if c=='y':
+        print (meetCodeGiver(x))    #OUTPUT 
+        pyperclip.copy(outputCode)
+        print("Code Copied to Clipboard, Opening Zoom, you're welcome :-)")
+        subprocess.Popen("C:\\Users\\home\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+        time.sleep(7)
+        keyboard.write(str(outputCode))  #Keyboard can only iterate strings!
+        print("Pasting the Code")
+        time.sleep(7)
+        print('Here goes the password:')
+        time.sleep(3)
+        keyboard.write("mesmpl")
+
+    else:
+        pass
+
     #All the TODOS, any additional features, will have to be coded here. 
