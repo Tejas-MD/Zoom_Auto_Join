9c823d763e9f684d1c58dbc4481305ed903d81bc
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-11-13 14:28:52 +0530

    Added Recommended Class Join Number. WORKING

--- Mes Teachers Code.py
+++ Mes Teachers Code.py
@@ -7,9 +7,11 @@ from coreFunc import coreFunc
 
 runFirstTimeStuff()  #One time, accepts 5 teachers initals and stores in 5 vars: x1,x2,x3,x4,x5
 
+recommendedNumberToJoin = 1
+
 while(True): #TODO: Wrongly looping, urgent fix needed! 
     print()
-    cmd=input('Waiting for your Orders, use -h for list of commands')
+    cmd=input('Waiting for your Orders, use -h for list of commands\n' + 'Recommended Number cause you are forgetful: ' + str(recommendedNumberToJoin))
     print()
     #TODO: Add commands list and teachers stored for this execution 
    
@@ -30,15 +32,20 @@ while(True): #TODO: Wrongly looping, urgent fix needed!
 
     else:
        if cmd=='1': #TODO: NOT WORKING, URGENT FIX NEEDED.   # So this is like the base station. It intelligently divertes you to the right function.
-           coreFunc(globalVars.x1)
+           coreFunc(globalVars.x1)    
+           recommendedNumberToJoin=recommendedNumberToJoin+1
        elif cmd=='2':
            coreFunc(globalVars.x2)
+           recommendedNumberToJoin=recommendedNumberToJoin+1
        elif cmd=='3':
             coreFunc(globalVars.x3)
+            recommendedNumberToJoin=recommendedNumberToJoin+1
        elif cmd=='4':
             coreFunc(globalVars.x4)
+            recommendedNumberToJoin=recommendedNumberToJoin+1
        elif cmd=='5':
             coreFunc(globalVars.x5)
+            recommendedNumberToJoin=recommendedNumberToJoin+1
        else:
            print()
            print("I Gave you so many commands to use but you have the audacity to enter the one I don't know about!")
