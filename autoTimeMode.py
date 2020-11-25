31bc92d49391c09b8672faa5ad0027eb1c0bbff7
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-11-25 12:20:51 +0530

    Minor Fine Tuning

--- autoTimeMode.py
+++ autoTimeMode.py
@@ -15,6 +15,7 @@ def timeClassComparer(hours,mins,arrayIndex,globalVarHere):
 
 
 def autoTimeModee():
+    print("I will take care of the classes myself, Tejas.")
     while(True):
         # year, month, day, hour, min = map(int, time.strftime("%Y %m %d %H %M").split())
         # if ((hour==11) and (min== 18)):
@@ -26,12 +27,12 @@ def autoTimeModee():
         timeClassComparer(10,2,0,globalVars.x1)
         timeClassComparer(11,2,1,globalVars.x2)
         timeClassComparer(12,2,2,globalVars.x3)
-        timeClassComparer(1,17,3,globalVars.x4)
-        timeClassComparer(2,17,4,globalVars.x5)
-        timeClassComparer(3,17,5,globalVars.x6)
+        timeClassComparer(13,17,3,globalVars.x4)
+        timeClassComparer(14,17,4,globalVars.x5)
+        timeClassComparer(15,17,5,globalVars.x6)
         
         # #test
-        # timeClassComparer(11,36,1,globalVars.x2) #WORKING!
+        # timeClassComparer(13,21,1,globalVars.x2) #WORKING!
 
 
 # autoTimeModee()
