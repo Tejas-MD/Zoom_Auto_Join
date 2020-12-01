71101eb8766f83a2435cddfa087207b3bddc4244
Tejas-MD <tejasmutalikdesai@gmail.com>
2020-12-01 11:09:31 +0530

    Implemented learnerBot's textfiles on CoreFunc.

--- coreFunc.py
+++ coreFunc.py
@@ -1,6 +1,20 @@
 import subprocess,time,pyperclip,keyboard,pyautogui
 from meetCodeSwitcher import meetCodeGiver
 
+def openZoomfromtxt():    #Using the Zoom Path TXT
+    zoomPathFile= open("ZoomPath.txt","r")
+    subprocess.Popen(zoomPathFile.read())
+    zoomPathFile.close()
+    # print('Please observe if ZOOM opens in 15 sec')
+    time.sleep(14)
+
+def accessCoods(FileName):
+    ObjectName= open(FileName,"r")
+    plusButtonX = int(ObjectName.read())
+    ObjectName.close()
+    return plusButtonX
+
+
 def coreFunc(initalsPlease):
     #Everyrhing down here is the core func. TODO: to be wrapped and kept somewhere and used if user command is a num btw 1-5(TeacherNumberCode.py)
     print()
@@ -16,27 +30,38 @@ def coreFunc(initalsPlease):
     pyperclip.copy(outputCode)
 
     #Open Zoom
-    print("Code Copied to Clipboard, Opening Zoom, you're welcome :-)")
-    openZoom = subprocess.Popen("C:\\Users\\home\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe")
+    print("Meeting Code Retrieved, Opening Zoom, you're welcome :-)")
+    openZoomfromtxt()
 
     #Wait until it opens 
     time.sleep(14)
 
-    #Click on the Join(+) Icon!
-    pyautogui.moveTo(590,283)
-    pyautogui.click()
 
-    time.sleep(2)
+    #Click on the Join(+) Icon!
+    print('Trying the + Button!')
+    plusButtonX=accessCoods('plusXCoordinate.txt')
+    plusButtonY=accessCoods('plusYCoordinate.txt')
+
+    pyautogui.moveTo(plusButtonX,plusButtonY)
+    pyautogui.click()
+    time.sleep(2)    #Zoom is obviously slower than my code
+
+
     # Type the meeting ID
     keyboard.write(str(outputCode))  # Keyboard can only iterate strings!
     print("Typing the Code")
     # pyautogui.moveTo(527,457) #Turn off video button
     # pyautogui.click() #Not required cause there's a zoom setting.
     time.sleep(1)
-    pyautogui.moveTo(707,498) #Join Button
+
+    #Clicking Join 
+    print('Lets Go!')
+    joinButtonX=accessCoods('joinXCoordinate.txt')  
+    joinButtonY=accessCoods('joinYCoordinate.txt')
+    pyautogui.moveTo(joinButtonX,joinButtonY)
     pyautogui.click()
-    time.sleep(5)
     
+
     keyboard.write("mesmpl")
     print("There you go, the password!")
     # pyautogui.moveTo(691,498) #Join Meeting Button after entering password   #Not Req cause it occurs at same coordinates
